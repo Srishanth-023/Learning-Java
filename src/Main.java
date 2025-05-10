f98a9177c0c144065d2edabdefc25e6257a0df31
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-10 20:47:14 +0530

    Variables

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,67 @@ public class Main {
 //        System.out.println("Buy me some 🫠");
 //        System.out.print("I am craving for that !");
 
-        // Variables
-        int age = 18;
-        double gpa = 9.48;
-        char grade = 'O';
-        boolean isStudent = true;
 
-        System.out.println("Your age is " + age);
-        System.out.println("Your gpa is " + gpa);
-        System.out.println("Your grade is " + grade);
-        System.out.println("Student ? " + isStudent);
+//        // Variables
+//        // Primitive
+//        int age = 18;
+//        double gpa = 9.48;
+//        char grade = 'O';
+//        boolean isStudent = true;
+//
+//        System.out.println("Your age is " + age);
+//        System.out.println("Your gpa is " + gpa);
+//        System.out.println("Your grade is " + grade);
+//        System.out.println("Student ? " + isStudent);
+//
+//        if (isStudent){
+//            System.out.println("Yes, you're a student");
+//        } else{
+//            System.out.println("You're not a student");
+//        }
+//
+//        // Reference
+//        String name = "Sri";
+//        String car = "Lamborghini";
+//
+//        System.out.println("Your name is " + name + " and you own a " + car);
 
-        if (isStudent){
-            System.out.println("Yes, you're a student");
-        } else{
-            System.out.println("You're not a student");
-        }
+
+        //
+
+
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
