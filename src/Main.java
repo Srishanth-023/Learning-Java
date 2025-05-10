ecb7116ce9389f17b6350dabda2cc5f2ac832d93
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-10 19:09:33 +0530

    Added a new line

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("I like Pizza !");
         System.out.print("They taste good.\n");
-        System.out.print("Buy me some 🫠");
+        System.out.println("Buy me some 🫠");
+        System.out.print("I am craving for that !");
     }
 }
