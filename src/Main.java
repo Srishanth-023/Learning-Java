a1118cb9e03994aa716aebaa6eb1f154ade309ab
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-10 21:25:29 +0530

    User input and Area of a triangle problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
+import javax.xml.transform.Source;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args){
 
 //        // Printing to the console
@@ -34,7 +36,48 @@ public class Main {
 //        System.out.println("Your name is " + name + " and you own a " + car);
 
 
-        //
+//        // User input
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//        System.out.print("Enter your gpa: ");
+//        double gpa = scanner.nextDouble();
+//        System.out.print("Are you a student: (true/false): ");
+//        boolean isStudent = scanner.nextBoolean();
+//
+//        System.out.println("Your name is " + name);
+//        System.out.println("Your age is " + age);
+//        System.out.println("Your gpa is " + gpa);
+//        if (isStudent){
+//            System.out.println("You are a student\n");
+//        } else{
+//            System.out.println("You're not !!\n");
+//        }
+//
+//        System.out.print("Enter the year you were born: ");
+//        int year = scanner.nextInt();
+//        scanner.nextLine();
+//        System.out.print("Enter the color you like: ");
+//        String color = scanner.nextLine();
+//
+//        System.out.println("You were born in " + age + " and the color you like is " + color);
+//
+//        scanner.close();
+
+//        // Area of a triangle
+//        Scanner scanner = new Scanner(System.in);
+//
+//        double width = 0, height = 0, area = 0;
+//        System.out.print("Enter the width: ");
+//        width = scanner.nextDouble();
+//        System.out.print("Enter the height: ");
+//        height = scanner.nextDouble();
+//
+//        area = width * height;
+//        System.out.println("The area of the triangle is: " + area + "cm²");
 
 
 
