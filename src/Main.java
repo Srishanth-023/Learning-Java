62614df49d94c1b7741e46d855b63f1819b0746f
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-10 20:39:10 +0530

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("I like Pizza !");
-        System.out.print("They taste good.\n");
-        System.out.println("Buy me some 🫠");
-        System.out.print("I am craving for that !");
+
+//        // Printing to the console
+//        System.out.println("I like Pizza !");
+//        System.out.print("They taste good.\n");
+//        System.out.println("Buy me some 🫠");
+//        System.out.print("I am craving for that !");
+
+        // Variables
+        int age = 18;
+        double gpa = 9.48;
+        char grade = 'O';
+        boolean isStudent = true;
+
+        System.out.println("Your age is " + age);
+        System.out.println("Your gpa is " + gpa);
+        System.out.println("Your grade is " + grade);
+        System.out.println("Student ? " + isStudent);
+
+        if (isStudent){
+            System.out.println("Yes, you're a student");
+        } else{
+            System.out.println("You're not a student");
+        }
+
     }
 }
