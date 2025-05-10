782d2d6d3d2a91dad54446838b38e35bce4f4bb0
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-10 18:50:03 +0530

    IDE files added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("I like Pizza !");
+        System.out.print("They taste good.\n");
+        System.out.print("Buy me some 🫠");
+    }
+}
