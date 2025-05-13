ef3a16d327b883797363ee2a1f9dbbeb34e46cd8
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-13 21:27:42 +0530

    Nested loops

--- src/Main.java
+++ src/Main.java
@@ -440,6 +440,41 @@ public class Main {
 //        }
 
 
+//        // Nested Loops
+//        for (int i = 1; i <= 3; i++){
+//            for (int j = 1; j <= 9; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        int rows;
+//        int columns;
+//        char symbol;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter the # of rows: ");
+//        rows = scanner.nextInt();
+//
+//        System.out.print("Enter the # of columns: ");
+//        columns = scanner.nextInt();
+//
+//        System.out.print("Enter the symbol: ");
+//        symbol = scanner.next().charAt(0);
+//
+//        System.out.println();
+//
+//        for (int i = 0; i < rows; i++){
+//            for (int j = 0; j < columns; j++){
+//                System.out.print(symbol);
+//            }
+//            System.out.println();
+//        }
+//
+//        scanner.close();
+
+
         //
 
 
