ea6512ad087d697509ad409ae86f6eb0aa75a436
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-13 19:09:16 +0530

    Logical operators

--- src/Main.java
+++ src/Main.java
@@ -326,6 +326,38 @@ public class Main {
 //        scanner.close();
 
 
+//        // Logical Operators
+//        double temp = 20;
+//        boolean isSunny = false;
+//
+//        if (temp >= 0 && temp <= 30 && isSunny){
+//            System.out.println("The weather is good !");
+//            System.out.println("It is SUNNY outside !!");
+//        } else if (temp >= 0 && temp <= 30 && !isSunny){
+//            System.out.println("The weather is good !");
+//            System.out.println("It is CLOUDY outside !!");
+//        } else if (temp < 0 || temp > 30){
+//            System.out.println("The weather is bad outside !!!");
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String username;
+//
+//        System.out.print("Enter your username: ");
+//        username = scanner.nextLine();
+//
+//        if (username.length() < 4 || username.length() > 12){
+//            System.out.println("Username must be within the limit of 4-12 characters !");
+//        } else if (username.contains(" ") || username.contains("_")){
+//            System.out.println("Username must not contain space or underscore !");
+//        } else{
+//            System.out.println("Welcome " + username);
+//        }
+//
+//        scanner.close();
+
+
         //
 
     }
